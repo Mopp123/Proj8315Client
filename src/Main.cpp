823e95f5eb04937571e8d277396d625fe6066326
Mopp123 <keahonen@gmail.com>
2024-09-17 21:40:22 +0300

    all sorts of reworking

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+#include "net/platform/web/WebClient.h"
+
+
 int main(int argc, const char** argv)
 {
     std::cout << "Working!!\n";
+
+    net::web::WebClient* client = new net::web::WebClient("ws://127.0.0.1:51421");
 
     return 0;
 }
---
+++ src/net/Client.cpp
@@ -0,0 +1,40 @@
+#include "Client.h"
+
+
+namespace net
+{
+    Client* Client::s_pInstance = nullptr;
+
+    void Client::addOnMessageEvent(int32_t messageType, OnMessageEvent* event)
+    {
+        if (_onMessageEvents.find(messageType) == _onMessageEvents.end())
+            _onMessageEvents.insert({ messageType, event });
+        else
+            pk::Debug::log(
+                "Attempted to add OnMessageEvent but event with same type (" + std::to_string(messageType) + ") already exists",
+                pk::Debug::MessageType::PK_ERROR
+            );
+    }
+
+    void Client::clearOnMessageEvents()
+    {
+        for (auto event : _onMessageEvents)
+            delete event.second;
+        _onMessageEvents.clear();
+
+        pk::Debug::log("OnMessageEvents cleared");
+    }
+
+    Client* Client::get_instance()
+    {
+        if (!s_pInstance)
+        {
+            pk::Debug::log("Attempted to access client instance, but the client didn't exist");
+            return nullptr;
+        }
+        else
+        {
+            return s_pInstance;
+        }
+    }
+}
