21da256cf30af9402365ff96ef800f9b703d38fe
Mopp123 <keahonen@gmail.com>
2024-11-28 00:27:48 +0200

    Added config file which is atm only used to specify server address

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,6 +7,7 @@
 #include "net/Client.h"
 #include "net/platform/web/WebClient.h"
 
+#include "../PortablePesukarhu/utils/FileUtils.h"
 #include <iostream>
 
 using namespace pk;
@@ -16,6 +17,7 @@ using namespace ui;
 
 int main(int argc, const char** argv)
 {
+
     // NOTE: ISSUES!
     // * need to create window and input manager to heap using
     // some "create func" like the other api/platform agnostic stuff..
@@ -33,7 +35,20 @@ int main(int argc, const char** argv)
         &inputManager
     );
 
-    net::Client* pClient = (net::Client*)(new net::web::WebClient("ws://127.0.0.1:51421"));
+    const std::string configPath = "assets/config.conf";
+    std::string configStr = load_text_file(configPath);
+    if (configStr.empty())
+    {
+        Debug::log(
+            "@main "
+            "Invalid config file: " + configPath + " "
+            "returned config data was empty",
+            Debug::MessageType::PK_FATAL_ERROR
+        );
+        return 0;
+    }
+    const std::string fullHost = "ws://" + configStr;
+    net::Client* pClient = (net::Client*)(new net::web::WebClient(fullHost));
     application.switchScene((Scene*)(new MainMenu));
     application.run();
 
